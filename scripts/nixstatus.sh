b3fa7e09606dbcbc8decc7dcf8c10ae8e3fbd229
nixos-laptop <configuration@nix-laptop>
2025-12-18 04:33:09 +0100

    Fixed bug in nixstatus where uncommitted files did not dirty  the state. Now, untracked files also dirty the state to align with Git's behaviour

--- scripts/nixstatus.sh
+++ scripts/nixstatus.sh
@@ -10,8 +10,8 @@ sudo git status --branch --short
 
 echo
 
-if ! sudo git diff --quiet || ! sudo git diff --cached --quiet; then
-  echo "⚠️  Uncommitted changes present"
+if sudo git status --porcelain | grep -q .; then
+  echo "⚠  Uncommitted changes present"
   exit 1
 else
   echo "✔ Working tree clean"
