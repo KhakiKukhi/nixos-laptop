4ec8aa529d26dc3cdf321b743d188fede0d508e6
nixos-laptop <configuration@nix-laptop>
2025-12-18 01:46:44 +0100

    Enable ssh-agent via home-manager

--- home.nix
+++ home.nix
@@ -6,25 +6,35 @@
 
   home.stateVersion = "24.05";
 
-    programs.bash = {
-      enable = true;
-
-      shellAliases = {
-        ll = "ls -lah";
-        gs = "git status";
-        ga = "git add";
-        gc = "git commit";
-        gp = "git push";
-      };
-
-     bashrcExtra = ''
-       export EDITOR=vim
-       export HISTCONTROL=ignoredups:erasedups
-       export HISTSIZE=10000
-       export HISTFILESIZE=20000
-
-       # nicer prompt (simple, readable)
-       PS1="[\u@\h \W]\\$ "
-     '';
+  programs.bash = {
+    enable = true;
+    
+    shellAliases = {
+      ll = "ls -lah";
+      gs = "git status";
+      ga = "git add";
+      gc = "git commit";
+      gp = "git push";
+      
+      nixrebuild = "sudo nixos-rebuild switch --flake /etc/nixos#laptop";
     };
+   
+    bashrcExtra = ''
+      export EDITOR=vim
+      export HISTCONTROL=ignoredups:erasedups
+      export HISTSIZE=10000
+      export HISTFILESIZE=20000
+      
+      # nicer prompt (simple, readable)
+      PS1="[\u@\h \W]\\$ "
+    '';
+  };
+
+  programs.ssh = {
+    enable = true;
+    addKeysToAgent = "yes";
+  };
+
+  services.ssh-agent.enable = true;
+
 }
