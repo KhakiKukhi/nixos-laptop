fb136096e1b8ed6fccb23e51017ea6afca11d6a3
nixos-laptop <configuration@nix-laptop>
2025-12-18 01:04:56 +0100

    Configure bash via home-manager

--- home.nix
+++ home.nix
@@ -6,6 +6,25 @@
 
   home.stateVersion = "24.05";
 
-  programs.bash.enable = true;
-}
+    programs.bash = {
+      enable = true;
 
+      shellAliases = {
+        ll = "ls -lah";
+        gs = "git status";
+        ga = "git add";
+        gc = "git commit";
+        gp = "git push";
+      };
+
+     bashrcExtra = ''
+       export EDITOR=vim
+       export HISTCONTROL=ignoredups:erasedups
+       export HISTSIZE=10000
+       export HISTFILESIZE=20000
+
+       # nicer prompt (simple, readable)
+       PS1="[\u@\h \W]\\$ "
+     '';
+    };
+}
