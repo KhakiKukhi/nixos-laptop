68317d76a5a891b084d3528c0786d8edf54215de
nixos-laptop <configuration@nix-laptop>
2025-12-19 01:56:11 +0100

    Enabled kwallet

--- configuration.nix
+++ configuration.nix
@@ -61,6 +61,8 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
+  services.kwallet.enable = true;
+
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
